ad0e643cd2d532c260c6c85de77087b52ac8ce41
catalin_gutu33 <cataline33k@gmail.com>
2025-12-16 18:52:11 +0200

    Add files via upload

---
+++ ex2_16_12_2025_CG.py
@@ -0,0 +1,8 @@
+a=input("a = ")
+b=input("b = ")
+cifre_a=sorted(a)
+cifre_b=sorted(b)
+if cifre_a==cifre_b:
+    print("Numerele sunt prietenoase")
+else:
+    print("Numerele nu sunt prietenoase")
