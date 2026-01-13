f10a30a89bea30794b2541ff537ea8c85b501103
catalin_gutu33 <cataline33k@gmail.com>
2026-01-13 19:25:21 +0200

    Pr5_13_01_25_CG.py

--- Modelare_Matematica/Pr5_13_01_25_CG.py
+++ Modelare_Matematica/Pr5_13_01_25_CG.py
@@ -1,36 +1,23 @@
-def zile_pana_la_varf(H, u, d, h0, incepe_dimineata=True):
-    zi=0
-    h=h0
-    while h<H:
-        zi+=1
-        if incepe_dimineata or zi>1:
-            h+=u
-            if h>=H:
-                break
-        h-=d
-        if h<0:
-            h=0
+# a) S-a pornit dimineața de la 0 m.
+H = 15
+h0 = 0
+u = 5
+d = 3
+N = max(1, (H - h0 + (u - d) - 1) // (u - d))
+print("Numarul de zile1:", N)
 
-    return zi
+# b) Ascensiunea începe dimineaţa de la înălţimea de 6 m.
+H = 15
+h0 = 6
+u = 5
+d = 3
+N = max(1, (H - h0 + (u - d) - 1) // (u - d))
+print("Numarul de zile2:", N)
 
-
-#a)Incepe dimineata de la 0m
-H=15
-u=5
-d=3
-h0=0
-
-rezultat_a = zile_pana_la_varf(H, u, d, h0, incepe_dimineata=True)
-print("a)Buburuza ajunge in varf in", rezultat_a, "zile")
-
-
-#b)Incepe dimineata de la 6m
-h0=6
-
-rezultat_b=zile_pana_la_varf(H, u, d, h0, incepe_dimineata=True)
-print("b)Buburuza ajunge in varf in", rezultat_b, "zile")
-
-
-# c)Incepe noaptea de la 6m
-rezultat_c = zile_pana_la_varf(H, u, d, h0, incepe_dimineata=False)
-print("c)Buburuza ajunge in varf in", rezultat_c, "zile")
+# c) Ascensiunea începe odată cu căderea nopţii.
+H = 15
+h0 = 3
+u = 5
+d = 3
+N = max(1, (H - h0 + (u - d) - 1) // (u - d))
+print("Numarul de zile:", N)
