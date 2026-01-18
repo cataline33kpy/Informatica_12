38a223f697d82414f79900541c06528015f8a3a0
catalin_gutu33 <cataline33k@gmail.com>
2026-01-18 21:01:13 +0200

    Delete ex2_16_12_2025_CG.py

--- ex2_16_12_2025_CG.py
+++
@@ -1,8 +0,0 @@
-a=input("a = ")
-b=input("b = ")
-cifre_a=sorted(a)
-cifre_b=sorted(b)
-if cifre_a==cifre_b:
-    print("Numerele sunt prietenoase")
-else:
-    print("Numerele nu sunt prietenoase")
