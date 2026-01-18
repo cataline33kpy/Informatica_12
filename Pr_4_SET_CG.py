6605d8dbf60b5e04e90a30b82d98cf71dd8f248c
catalin_gutu33 <cataline33k@gmail.com>
2026-01-18 20:59:38 +0200

    Delete Pr_4_SET_CG.py

--- Pr_4_SET_CG.py
+++
@@ -1,8 +0,0 @@
-A=[1,2,3,'A','B','C']
-n=len(A)
-for i in range(2**n):
-    submultime=[]
-    for j in range(n):
-        if (i>>j)&1:
-            submultime.append(A[j])
-    print(set(submultime))     
