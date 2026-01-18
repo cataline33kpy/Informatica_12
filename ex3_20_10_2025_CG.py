6c771f6343bc00852c8d2a7fe94b1c18fbab4fc9
catalin_gutu33 <cataline33k@gmail.com>
2026-01-18 21:01:30 +0200

    Delete ex3_20_10_2025_CG.py

--- ex3_20_10_2025_CG.py
+++
@@ -1,6 +0,0 @@
-import random
-nr=random.randint(0,999999999)
-print('Numarul generat este:',nr)
-sir=str(nr)
-for cifra in range(10):
-    print(f"Cifra {cifra} apare de {sir.count(str(cifra))} ori")
