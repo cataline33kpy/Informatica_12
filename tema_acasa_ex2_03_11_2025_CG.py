3e1d6a63231875ccfb051119785dbdc645d7b0d6
catalin_gutu33 <cataline33k@gmail.com>
2026-01-18 21:03:20 +0200

    Delete tema_acasa_ex2_03_11_2025_CG.py

--- tema_acasa_ex2_03_11_2025_CG.py
+++
@@ -1,9 +0,0 @@
-import random
-for i in range(10):
-    variante=[]
-    while len(variante)<5:
-        n = random.randint(1, 36)
-        if n not in variante:
-            variante.append(n)
-    variante.sort()
-    print(f"Varianta {i+1}: {variante}")
